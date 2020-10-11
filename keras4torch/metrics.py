a472c1b1ee7fcf911c153f8b02ac6bed5a3c1fca
pengzhangzhi <1445939286@qq.com>
2020-10-11 21:19:33 +0800

    Update metrics.py

--- keras4torch/metrics.py
+++ keras4torch/metrics.py
@@ -1,6 +1,7 @@
 from operator import rshift
 import torch
-
+import torch.nn as nn
+import math
 # For categorical classification
 def accuracy(y_pred, y_true):
     right_cnt = (y_pred.argmax(-1) == y_true).sum()
@@ -20,14 +21,25 @@ def f1_score(y_pred, y_true):
 
 
 # For Regression
+
 def mean_squared_error(y_pred, y_true):
-    pass
+    with torch.no_grad():
+        y_pred, y_true=torch.tensor(y_pred), torch.tensor(y_true)
+        loss = nn.MSELoss()
+        return loss(y_pred, y_true).item()
+
 
 def mean_absolute_error(y_pred, y_true):
-    pass
+    with torch.no_grad():
+        y_pred, y_true = torch.tensor(y_pred), torch.tensor(y_true)
+        loss = nn.L1Loss()
+        return loss(y_pred, y_true).item()
 
 def mean_absolute_percentage_error(y_pred, y_true):
-    pass
+    with torch.no_grad():
+        y_pred, y_true = torch.tensor(y_pred), torch.tensor(y_true)
+        return (torch.mean(torch.abs((y_true - y_pred) / y_true)) * 100).item()
 
-def root_mean_square_error(y_pred, y_true):
-    pass
+
+def root_mean_squared_error(y_pred, y_true):
+    return math.sqrt(mean_squared_error(y_pred, y_true))
