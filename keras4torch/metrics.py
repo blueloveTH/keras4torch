7022a442c11f30edf39bc13b5150ac7f8c0f2fda
blueloveTH <blueloveTH@qq.com>
2020-10-12 19:07:27 +0800

    update metrics

--- keras4torch/metrics.py
+++ keras4torch/metrics.py
@@ -1,33 +1,69 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from abc import abstractclassmethod
+
+class Metric():
+    def __init__(self) -> None:
+        pass
+    '''
+    @abstractclassmethod
+    def get_abbr(self) -> str:
+        pass
+    '''
 
 # For categorical classification
-def accuracy(y_pred, y_true):
-    right_cnt = (y_pred.argmax(-1) == y_true).sum()
-    return right_cnt * 1.0 / y_true.shape[0]
+class Accuracy(Metric):
+    def __init__(self) -> None:
+        super(Accuracy, self).__init__()
 
-# For binary classification
-def binary_accuracy(y_pred, y_true):
-    right_cnt = (torch.round(y_pred) == y_true).sum()
-    return right_cnt * 1.0 / y_true.shape[0]
+    def __call__(self, y_pred, y_true):
+        right_cnt = (y_pred.argmax(-1) == y_true).sum()
+        return right_cnt * 1.0 / y_true.shape[0]
 
-def roc_auc(y_pred, y_true):
-    pass
+class BinaryAccuracy(Metric):
+    def __init__(self) -> None:
+        super(BinaryAccuracy, self).__init__()
 
-def f1_score(y_pred, y_true):
-    pass
+    def __call__(self, y_pred, y_true):
+        right_cnt = (torch.round(y_pred) == y_true).sum()
+        return right_cnt * 1.0 / y_true.shape[0]
+
+class ROC_AUC(Metric):
+    def __init__(self) -> None:
+        super(ROC_AUC, self).__init__()
+        raise NotImplementedError()
+
+    def __call__(self, y_pred, y_true):
+        pass
+
 
 
 # For Regression
 
-def mean_squared_error(y_pred, y_true):
-    return nn.MSELoss()(y_pred, y_true)
+class MeanSquaredError(Metric):
+    def __init__(self) -> None:
+        super(MeanSquaredError, self).__init__()
 
-def mean_absolute_error(y_pred, y_true):
-    return nn.L1Loss()(y_pred, y_true)
+    def __call__(self, y_pred, y_true):
+        return F.mse_loss(y_pred, y_true)
 
+class MeanAbsoluteError(Metric):
+    def __init__(self) -> None:
+        super(MeanAbsoluteError, self).__init__()
+
+    def __call__(self, y_pred, y_true):
+        return F.l1_loss(y_pred, y_true)
+
+class RootMeanSquaredError(Metric):
+    def __init__(self) -> None:
+        super(RootMeanSquaredError, self).__init__()
+
+    def __call__(self, y_pred, y_true):
+        return torch.sqrt(F.mse_loss(y_pred, y_true))
+
+
+'''
 def mean_absolute_percentage_error(y_pred, y_true):
     return torch.mean(torch.abs((y_true - y_pred) / y_true)) * 100
-
-def root_mean_squared_error(y_pred, y_true):
-    return torch.sqrt(nn.MSELoss()(y_pred, y_true))
+'''
