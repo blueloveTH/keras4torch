2f91faa7de1055ac556581ea96d4fd8839175d9b
blueloveTH <blueloveTH@qq.com>
2021-01-12 21:50:36 +0800

    Update __init__.py

--- keras4torch/utils/__init__.py
+++ keras4torch/utils/__init__.py
@@ -1,4 +1,4 @@
-from multiprocessing import cpu_count
+from multiprocessing import cpu_count as _cpu_count
 
 from ._to_tensor import to_tensor
 from .data import *
@@ -6,5 +6,5 @@ from .data import *
 
 def _get_num_workers(num_workers):
     if num_workers == -1:
-        return cpu_count() - 1
+        return _cpu_count() - 1
     return num_workers
