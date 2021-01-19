1dea13da6ceb9a26f4bc0f6abffaefcc696c4d04
blueloveTH <blueloveTH@qq.com>
2021-01-19 19:56:17 +0800

    up

--- keras4torch/metrics.py
+++ keras4torch/metrics.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 class Metric():
     def __init__(self) -> None:
@@ -10,7 +11,6 @@ class Metric():
         raise NotImplementedError()
 
 class Accuracy(Metric):
-
     def __call__(self, y_pred, y_true):
         if y_pred.shape[-1] == 1 or y_pred.dim() == 1:
             return binary_accuracy(y_pred, y_true)
@@ -26,6 +26,39 @@ def binary_accuracy(y_pred, y_true, activation=torch.sigmoid):
     return (torch.round(activation(y_pred)) == y_true).float().mean()
 
 
+
+class ROC_AUC(Metric):
+    """
+    Fast AUC implementation collected from Kaggle.
+    
+    By default, this metric will output the same result as `sklearn.metrics.roc_auc_score`
+
+    See reference: https://www.kaggle.com/c/riiid-test-answer-prediction/discussion/208031
+    """
+    def __init__(self, activation=torch.sigmoid):
+        from scipy.stats import rankdata
+        self.sort_fn = rankdata
+        self.activation = activation
+
+    @staticmethod
+    def _auc(y_true, pred_ranks):
+        n_pos = np.sum(y_true).astype('int64')
+        n_neg = len(y_true) - n_pos
+        left = pred_ranks[y_true==1].sum() - n_pos * (n_pos + 1) / 2
+        return left / (n_pos * n_neg)
+
+    def __call__(self, y_pred, y_true):
+        y_true = y_true.cpu().numpy().reshape(-1)
+        y_pred = self.activation(y_pred).cpu().numpy().reshape(-1)
+        pred_ranks = self.sort_fn(y_pred)
+        result = self._auc(y_true, pred_ranks)
+        return torch.tensor(result, dtype=torch.float32, device='cpu')
+
+    def get_abbr(self) -> str:
+        return 'auc'
+
+
+
 class SklearnMetric(Metric):
     def __init__(self, activation=None) -> None:
         super(SklearnMetric, self).__init__()
@@ -39,26 +72,26 @@ class SklearnMetric(Metric):
         self.score_fn = self.get_score_fn(sklearn_metrics)
 
     def __call__(self, y_pred, y_true):
-        _device = y_pred.device
         if self.activation is not None:
             y_pred = self.activation(y_pred)
         y_true = y_true.cpu().numpy()
         y_pred = y_pred.cpu().numpy()
-        return torch.tensor(self.score_fn(y_true, y_pred), dtype=torch.float32, device=_device)
+        return torch.tensor(self.score_fn(y_true, y_pred), dtype=torch.float32, device='cpu')
 
     def get_score_fn(self, sklearn_metrics):
         raise NotImplementedError()
 
 
-class ROC_AUC(SklearnMetric):
+class ROC_AUC_2(SklearnMetric):
     def __init__(self, activation=torch.sigmoid) -> None:
-        super(ROC_AUC, self).__init__(activation=activation)
+        super(ROC_AUC_2, self).__init__(activation=activation)
 
     def get_score_fn(self, sklearn_metrics):
         return sklearn_metrics.roc_auc_score
 
     def get_abbr(self) -> str:
         return 'auc'
+
 
 class F1_Score(SklearnMetric):
     def __init__(self, activation=torch.sigmoid) -> None:
